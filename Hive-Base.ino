5835428a2a63d014f9cdc2f0e6101b7bf3b10269
amackp <abe@lifefuels.co>
2021-04-06 11:56:37 -0400

    initial commit

---
+++ Hive-Base.ino
@@ -0,0 +1,220 @@
+#include <HX711.h>
+
+#include <DHT.h>
+#include <DHT_U.h>
+#include  "Hive.h"
+
+#define SOUND_PIN   A1
+#define DHTPIN      7
+#define DHTTYPE     DHT11   
+#define PELTIER_PIN 9
+
+#define TEMP_HYSTERYSIS 3
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+HiveData hive_data;
+
+const int LOADCELL_DOUT_PIN = 5;
+const int LOADCELL_SCK_PIN = 6;
+
+#define SCALE_CAL_VALUE -7000.f
+
+HX711 scale;
+
+uint32_t delayMS;
+
+void array_to_string(byte array[], unsigned int len, char buffer[])
+{
+    for (unsigned int i = 0; i < len; i++)
+    {
+        byte nib1 = (array[i] >> 4) & 0x0F;
+        byte nib2 = (array[i] >> 0) & 0x0F;
+        buffer[i*2+0] = nib1  < 0xA ? '0' + nib1  : 'A' + nib1  - 0xA;
+        buffer[i*2+1] = nib2  < 0xA ? '0' + nib2  : 'A' + nib2  - 0xA;
+    }
+    buffer[len*2] = '\0';
+}
+
+void hive_push_data(){
+  
+    String payload = HIVE_INSIDE_TEMP + String(hive_data.inside_temperature_c)+",";
+    //String cmd = "AT+SEND=501,"+String(payload.length()) +","+ payload;
+    //lora_send_cmd(cmd, NULL, 1);
+    //delay(500);
+
+    payload += HIVE_INSIDE_HUMID + String(hive_data.inside_humidity)+",";
+    //cmd = "AT+SEND=501,"+String(payload.length()) +","+ payload;
+    //lora_send_cmd(cmd, NULL, 1);
+    //delay(500);
+
+    payload += HIVE_OUTSIDE_TEMP + String(hive_data.outside_temperature_c)+",";
+    //cmd = "AT+SEND=501,"+String(payload.length()) +","+ payload;
+    //lora_send_cmd(cmd, NULL, 1);
+    //delay(500);
+
+    payload += HIVE_OUTSIDE_HUMID + String(hive_data.outside_humidity)+",";
+    //cmd = "AT+SEND=501,"+String(payload.length()) +","+ payload;
+    //lora_send_cmd(cmd, NULL, 1);
+    //delay(500);
+
+    payload += HIVE_SOUND_LEVEL + String(hive_data.sound_db)+",";
+   // cmd = "AT+SEND=501,"+String(payload.length()) +","+ payload;
+    //lora_send_cmd(cmd, NULL, 1);
+    //delay(500);
+
+    payload += HIVE_BAT_LEVEL + String(hive_data.bat_charge)+",";
+    //cmd = "AT+SEND=501,"+String(payload.length()) +","+ payload;
+    //lora_send_cmd(cmd, NULL, 1);
+    //delay(500);
+
+    payload += HIVE_BAT_STATUS + String(hive_data.bat_status)+",";
+    //cmd = "AT+SEND=501,"+String(payload.length()) +","+ payload;
+   // lora_send_cmd(cmd, NULL, 1);
+    //delay(500);
+
+    payload += HIVE_FAN_STATUS + String(hive_data.fan_status)+",";
+
+    payload += HIVE_CMD_SETTEMP + String(hive_data.set_temp_c)+",";
+    
+    payload += HIVE_WEIGHT + String(hive_data.raw_weight)+",";
+
+    
+
+    
+    String cmd = "AT+SEND=501,"+String(payload.length()) +","+ payload; 
+    lora_send_cmd(cmd, NULL, 1);
+    //delay(500);
+}
+
+void print_data_info(){
+
+  Serial.println(F("------------------------------------"));
+  
+  Serial.println(F("Sensor and Device Statuses"));
+  Serial.print  (F("Temperature: ")); Serial.print(hive_data.inside_temperature_c);Serial.println(F(" C"));
+  Serial.print  (F("Humidity: ")); Serial.print(hive_data.inside_humidity);Serial.println(F(" %"));
+  Serial.print  (F("Set temp: ")); Serial.print(hive_data.set_temp_c);Serial.println(F(" C"));
+  Serial.print  (F("Fan status: ")); Serial.println(hive_data.fan_status==0?"Fan off":"Fan active");
+  Serial.print  (F("Scale value: ")); Serial.print(hive_data.raw_weight);Serial.println(F(" lbs"));
+  Serial.print  (F("Battery charge: ")); Serial.print(hive_data.bat_charge);Serial.println(F(" %"));
+  Serial.print  (F("Battery Status: ")); Serial.println(hive_data.bat_status==0?"Not charging":"Charging");
+  Serial.print  (F("Sound level: ")); Serial.print(hive_data.sound_db);Serial.println(F(" dB"));
+  
+  Serial.println(F("------------------------------------"));
+
+  
+}
+
+void scale_calibrate(){
+  
+  scale.set_scale(-7000.f);
+  scale.tare();
+
+  while(1){
+      Serial.print("read average: \t\t");
+      Serial.println(scale.get_units());       // print the average of 20 readings from the ADC
+      //scale.tare();
+      delay(250);
+  }
+
+  
+}
+
+void scale_init(){
+  Serial.println("Before setting up the scale:");
+  Serial.print("read: \t\t");
+  Serial.println(scale.read());      // print a raw reading from the ADC
+
+  Serial.print("read average: \t\t");
+  Serial.println(scale.read_average(20));   // print the average of 20 readings from the ADC
+
+  Serial.print("get value: \t\t");
+  Serial.println(scale.get_value(5));   // print the average of 5 readings from the ADC minus the tare weight (not set yet)
+
+  Serial.print("get units: \t\t");
+  Serial.println(scale.get_units(5), 1);  // print the average of 5 readings from the ADC minus tare weight (not set) divided
+            // by the SCALE parameter (not set yet)
+
+  scale.set_scale(-2280.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
+  scale.tare();               // reset the scale to 0
+
+  delay(5000);
+  Serial.println("After setting up the scale:");
+
+  Serial.print("read: \t\t");
+  Serial.println(scale.read());                 // print a raw reading from the ADC
+
+  Serial.print("read average: \t\t");
+  Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
+
+  Serial.print("get value: \t\t");
+  Serial.println(scale.get_value(5));    // print the average of 5 readings from the ADC minus the tare weight, set with tare()
+
+  Serial.print("get units: \t\t");
+  Serial.println(scale.get_units(5), 1);        // print the average of 5 readings from the ADC minus tare weight, divided
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(19200);
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.set_scale(-7000.f);
+  scale.tare();
+  lora_configure();
+  dht.begin();
+  pinMode(PELTIER_PIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+     //Get temperature event and print its value.
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  hive_data.inside_temperature_c = event.temperature;
+  hive_data.outside_temperature_c = event.temperature;
+  
+  dht.humidity().getEvent(&event);
+  hive_data.inside_humidity = event.relative_humidity;
+  hive_data.outside_humidity = event.relative_humidity;
+  
+  hive_data.sound_db = analogRead(SOUND_PIN);
+  hive_data.bat_charge = 90;
+  hive_data.bat_status = 0;
+  hive_data.raw_weight = scale.get_units(5);
+
+  String rx = lora_parse_rx(lora_recieve());
+  String cmd = rx.substring(0, HEADER_LENGTH);
+  String payload = rx.substring(HEADER_LENGTH);
+
+  if(cmd.equals(HIVE_CMD_FETCH)){
+    hive_push_data();
+  }
+  else if (cmd.equals(HIVE_CMD_TARE)){
+    scale.tare();
+    hive_data.raw_weight = scale.get_units(5);
+    payload = HIVE_WEIGHT + String(hive_data.raw_weight)+",";
+    String cmd = "AT+SEND=501,"+String(payload.length()) +","+ payload; 
+    lora_send_cmd(cmd, NULL, 1);
+  }
+  else if (cmd.equals(HIVE_CMD_SETTEMP)){
+    hive_data.set_temp_c = payload.toInt();
+  }
+
+  if(hive_data.inside_temperature_c - hive_data.set_temp_c > TEMP_HYSTERYSIS){
+    //turn on peltier
+    hive_data.fan_status = 1;
+    digitalWrite(PELTIER_PIN, HIGH);   
+  }
+  else{
+    //turn off peltier
+    hive_data.fan_status = 0;
+    digitalWrite(PELTIER_PIN, LOW); 
+  }
+  print_data_info();
+
+  
+  scale.power_down();              // put the ADC in sleep mode
+  delay(1000);
+  scale.power_up();
+}
---
+++ lora.ino
@@ -0,0 +1,204 @@
+#include <SoftwareSerial.h>
+#include "Hive.h"
+#define COMMS_TIMEOUT 5000
+SoftwareSerial lora(2,3);
+
+String lora_recieve(){
+  String rx_string;
+  
+  if(lora.available()){
+   
+    rx_string = lora.readStringUntil('\n');
+    
+    if(rx_string.length() == 0){
+      //Serial.println("No repsonse!");
+    }
+    else{
+      //Serial.print("New Rx, len: ");Serial.println(rx_string.length());
+      //Serial.print("Rx: ");Serial.println(rx_string);
+      return rx_string;
+    }
+  }
+  return String("No Rx");
+}
+
+
+//rx fromat +RCV=<ADDR>,<LENGTH>,<MSG>,<RSSI>,<SNR>
+//ex +RCV=50,5,HELLO,-99,40
+String lora_parse_rx(String message_in){
+  if(message_in.equals("No Rx")){
+    //empty string input, ignore
+    return String(NULL);
+  }
+  String header = message_in.substring(0,5);
+  if(!header.equals("+RCV=")){
+    Serial.println("Error parsing message, invalid header!");
+    Serial.println(message_in);
+    return String(NULL);
+  }
+  
+  int index_start = 5;
+  int index_end = 5;
+  for( ; index_end < message_in.length(); index_end++){
+    if(message_in.charAt(index_end) == ','){
+      break;
+    }
+  }
+  if(index_end == message_in.length()){
+    Serial.println("Error tx addr, no comma found!");
+    Serial.println(message_in);
+    return String(NULL);
+  }
+  String tx_addr = message_in.substring(index_start, index_end);
+  index_end++;
+  index_start = index_end;
+  Serial.print("New MSG - TX Addr: ");Serial.print(tx_addr);Serial.print(" | ");
+
+  for( ; index_end < message_in.length(); index_end++){
+    if(message_in.charAt(index_end) == ','){
+      break;
+    }
+  }
+  if(index_end == message_in.length()){
+    Serial.println("Error parsing msg length, no comma found!");
+    Serial.println(message_in);
+    return String(NULL);
+  }
+  String msg_length = message_in.substring(index_start, index_end);
+  index_end++;
+  index_start = index_end;
+  Serial.print("Msg Length: ");Serial.print(msg_length);Serial.print(" | ");
+
+  for( ; index_end < message_in.length(); index_end++){
+    if(message_in.charAt(index_end) == ','){
+      break;
+    }
+  }
+  if(index_end == message_in.length()){
+    Serial.println("Error parsing msg data, no comma found!");
+    Serial.println(message_in);
+    return String(NULL);
+  }
+  String msg_data = message_in.substring(index_start, index_end);
+  index_end++;
+  index_start = index_end;
+  Serial.print("Msg Data: ");Serial.print(msg_data);Serial.print(" | ");
+
+  for( ; index_end < message_in.length(); index_end++){
+    if(message_in.charAt(index_end) == ','){
+      break;
+    }
+  }
+  if(index_end == message_in.length()){
+    Serial.println("Error parsing rssi, no comma found!");
+    Serial.println(message_in);
+    return String(NULL);
+  }
+  String rssi = message_in.substring(index_start, index_end);
+  index_end++;
+  index_start = index_end;
+  Serial.print("RSSI: ");Serial.print(rssi);Serial.print(" | ");
+
+  String snr = message_in.substring(index_start);
+  Serial.print("SNR: ");Serial.println(rssi);
+
+  return msg_data;  
+}
+
+bool lora_send_cmd(String cmd_str, String * output, int retries){
+  String ret_str;
+  char ret_char;
+  
+  for(uint8_t i = 0; i < retries; i++){
+
+    lora.println(cmd_str);
+    Serial.print("Sent cmd: ");Serial.println(cmd_str);
+    delay(100);
+    int ctr = 0;
+    while(!lora.available()){
+      ctr++;// wait for device to respond
+      delay(1);
+      if(ctr > COMMS_TIMEOUT){
+        Serial.println("Timeout while waiting for device!");
+        break;
+      }
+    }
+    if(lora.available()){
+      ret_str = lora.readStringUntil('\n');
+    }
+
+    if(ret_str.length() == 0){
+      Serial.println("No repsonse!");
+    }
+    else{
+      Serial.print("Ret length: ");Serial.println(ret_str.length());
+      Serial.print("Return: ");Serial.println(ret_str);
+      return true;
+    }
+    ret_str.remove(0, ret_str.length());  //clean input string
+    delay(500);
+  }
+  return false;
+}
+
+bool lora_send_and_verify(String cmd_str, uint8_t retries){
+
+  String exp_ret = "+OK\r";
+  String ret_str;
+  char ret_char;
+  
+  for(uint8_t i = 0; i < retries; i++){
+    lora.println(cmd_str);
+    Serial.print("Sent cmd: ");Serial.println(cmd_str);
+    delay(100);
+    int ctr = 0;
+    while(!lora.available()){
+      ctr++;// wait for device to respond
+      delay(1);
+      if(ctr > COMMS_TIMEOUT){
+        Serial.println("Timeout while waiting for device!");
+        break;
+      }
+    }
+    if(lora.available()){
+      ret_str = lora.readStringUntil('\n');
+    }
+
+    if(ret_str.length() == 0){
+      Serial.println("No repsonse!");
+    }
+    else{
+      Serial.print("Ret length: ");Serial.println(ret_str.length());
+      Serial.print("Return: ");Serial.println(ret_str);
+      Serial.print("Match = ");Serial.println(ret_str.equals(exp_ret));
+      if(ret_str.equals(exp_ret)){
+        return true;
+      }
+    }
+    ret_str.remove(0, ret_str.length());  //clean input string
+    delay(500);
+  }
+  return false;
+}
+
+bool lora_configure(){
+  bool low_baud = false;
+  lora.begin(19200);
+  while(!lora_send_and_verify("AT+IPR=19200", 1)){
+    lora.end();
+    delay(100);
+    if(low_baud){
+      lora.begin(9600);
+    }
+    else{
+      lora.begin(19200);
+    }
+    low_baud = !low_baud;
+  }
+  lora_send_and_verify("AT+PARAMETER=9,7,1,4", 5);
+  lora_send_and_verify("AT+BAND=915000000", 5);
+  lora_send_and_verify("AT+NETWORKID=7", 5);
+  lora_send_and_verify("AT+ADDRESS=500", 5);
+  //lora_send_and_verify("AT+CPIN=FABC0002EEDCAA90FABC0002EEDCAA90", 5);  
+  lora.setTimeout(COMMS_TIMEOUT);
+}
